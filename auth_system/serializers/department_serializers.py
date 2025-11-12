326261b0a01f0cd1a68d95d53ea9799657f2b900
[xyz123] <[xyz123@gamil.com]>
2025-11-12 16:23:45 +0530

    udpate senitze function for serilser

--- auth_system/serializers/department_serializers.py
+++ auth_system/serializers/department_serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from auth_system.models.department import Department
+from comman.utils.serielizer_input_sentizer import validate_and_sanitize
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,3 +13,7 @@ class DepartmentSerializer(serializers.ModelSerializer):
             "updated_at",
             "deleted_at",
         )
+    def validate(self, attrs):
+       
+        attrs = validate_and_sanitize(attrs)  # Call the shared helper function
+        return attrs
